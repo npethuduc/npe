17f8650000736c6f7e1303cf86216b7ce0e89936
Trần Đình Gia Hải <60392917+giathyex@users.noreply.github.com>
2022-06-09 09:34:10 +0700

    .

--- progress/app.js
+++ progress/app.js
@@ -98,16 +98,12 @@ function renderList3(doc) {
 }
 
 // Press Enter to go
-$("form").on("submit", function(e) {
-    e.preventDefault();
-});
-
-$(function() {
-    $("input").on("keydown", function(e) {
-        if (e.keyCode === 13) {
-            myFunction()
-        }
-    });
+var numb = document.getElementById("numb");
+numb.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") { //checks whether the pressed key is "Enter"
+        myFunction();
+        alert('Enter');
+    }
 });
 
 function myFunction() {
